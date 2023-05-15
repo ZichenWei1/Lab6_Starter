3cf9e940bbcda221f0bb7f494be577cfac25b9cb
ZichenWei1 <z1wei@ucsd.edu>
2023-05-14 23:40:06 -0700

    version1

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,10 +8,14 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+    this.attachShadow({ mode: "open" });
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
+    let article=document.getElementsByClassName("article")
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+    let style = document.getElementsByClassName("style")
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
+    this.shadowRoot.append(style, article);
   }
 
   /**
